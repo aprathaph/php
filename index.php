d79a2196eacd86ba488cf631ad7f3af6d0495871
aprathaph <aprathaph@gmail.com>
2017-07-06 04:19:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h10>PHP is Fun!</h10>";
+echo "<h2>PHP is Fun!</h2>";
 echo "Hello world!<br>";
 echo "I'm about to learn PHP!<br>";
 echo "This ", "string ", "was ", "made ", "with multiple parameters.<br>";
