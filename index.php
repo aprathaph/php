1b718fea2030e3442e52e6ed42f93209949ea076
aprathaph <aprathaph@gmail.com>
2017-07-06 04:18:04 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h10>PHP is Fun!</h10>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.<br>";
+echo "PCF is huge and need to understand very carefully.";
+?>
